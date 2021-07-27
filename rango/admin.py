45255b00da3a986f8c0d351fbce77a3768bbd0d0
Yuguan Qin <2517865q@student.gla.ac.uk>
2021-07-28 03:34:05 +0800

    Chapter 9 Done

--- rango/admin.py
+++ rango/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from rango.models import Category, Page
-# Register your models here.
+from rango.models import Category, Page, UserProfile
+
 
 
 
@@ -13,3 +13,4 @@ class PageAdmin(admin.ModelAdmin):
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -17,6 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+LOGIN_URL = 'rango:login'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -88,6 +89,10 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -95,6 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {'min_length': 6}
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
