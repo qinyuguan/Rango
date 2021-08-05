fcbc50d360968910b4dd13832f5a7c952f1ffaf6
Yuguan Qin <2517865q@student.gla.ac.uk>
2021-08-05 18:09:58 +0800

    update frontend

--- rango/views.py
+++ rango/views.py
@@ -122,6 +122,9 @@ def register(request):
 
 
 def user_login(request):
+    if request.user.is_authenticated:
+        return redirect('rango:index')
+
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
